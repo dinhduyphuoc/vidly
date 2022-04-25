c22d9573cdd5190262933ab88e46bd32f3ed8c6e
iphuoc0309 <ddphuoc19@apcs.fitus.edu.vn>
2022-04-25 11:41:03 +0700

    Hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import { getGenres } from "./services/fakeGenreService";
+import { getMovies } from "./services/fakeMovieService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    movies: [],
+    genres: [],
+    pageSize: 4,
+  };
+
+  componentDidMount() {
+    this.setState({ movies: getMovies(), genres: getGenres() });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Movies />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
